100241f3d10c0c3785c0c9cc4f9c58694a3bfbac
Ivan <149413900+Flamenc0@users.noreply.github.com>
2024-05-30 21:39:28 +0300

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,16 +1,20 @@
-// Вместо нашего адреса вставь свой
-// Можно ещё не удалять адреса с localhost, но мы удалили для краткости
-const allowedCors = ["https://flamenco-frontend.nomorepartiesco.ru/", "localhost"];
-
-function cors(req, res, next) {
-  const { origin } = req.headers;
-
-  if (allowedCors.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
+const allowedCors = [
+    'https://flamenco-frontend.nomorepartiesco.ruru',
+    'flamenco-frontend.nomorepartiesco.ru',
+    'http://localhost:3000/',
+    'https://practicum.yandex.ru',
+    'https://students-projects.ru',
+    'http://localhost:3000',
+    'http://localhost:3001/', 
+  ];
+  function corse(req, res, next) {
+      const { origin } = req.headers; 
+      if (allowedCors.includes(origin)) { 
+        res.header('Access-Control-Allow-Origin', origin);
+      }
+      res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+      res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+      next(); 
   }
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
-  next();
-}
-
-module.exports = cors;
+  
+  module.exports = corse;
